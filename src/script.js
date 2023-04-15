d5164448d640736db65149270281260d94e9ce05
Imad Fayad <imad.fayad.if@gmail.com>
2023-04-15 16:17:40 +0200

    add dark mode

--- src/script.js
+++ src/script.js
@@ -1,3 +1,8 @@
 'use strict';
 
-console.log('hello');
+const themeBtn = document.querySelector('.theme-btn');
+const html = document.querySelector('html');
+
+themeBtn.addEventListener('click', function () {
+  html.dataset.theme = html.dataset.theme === 'light' ? 'dark' : 'light';
+});
